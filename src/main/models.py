995b4df07f062c7b725f570977dc8d4d0021eb95
Chayanin Punjakunaporn <herofactory.th@gmail.com>
2017-11-18 14:46:30 +0700

    write model product

--- src/main/models.py
+++ src/main/models.py
@@ -3,4 +3,15 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+# TODO (doing): model Product
+# No proving correctly
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    stock = models.IntegerField()
+    description = models.TextField()
+    def __str__(self):
+        return self.name
+
+# TODO: model User
+# TODO: model Order
