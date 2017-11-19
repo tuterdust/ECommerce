ae3b0042d309e3785af7264e3174f7e4f691444e
Goofy <goofy_clunctia@hotmail.com>
2017-11-20 01:49:42 +0900

    not done User model

--- src/main/models.py
+++ src/main/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # TODO (doing): model Product
 # No proving correctly
@@ -15,10 +16,15 @@ class Product(models.Model):
         return self.name
 
 # TODO: model User
+class User(models.Model):
+    email = models.CharField(max_length=100)
+    password = models.CharFiled(max_length=100)
+    order = ArrayField(models.ForeignKey(Order))
+    cart = ArrayField(models.ForeignKey(Product))
+
 
 # TODO: model Order
 class Order(models.Model):
     order_list = ArrayField(models.ForeignKey(Product))
     amount = ArrayField(models.IntegerField())
     date = IntegerField()
-    # TODO: DateTimeField for date ordering
