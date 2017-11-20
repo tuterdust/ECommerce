913cc0685c548a4a1cd0f3597d224e876c59ead4
Chayanin Punjakunaporn <herofactory.th@gmail.com>
2017-11-20 08:26:52 +0700

    Update models.py

--- src/main/models.py
+++ src/main/models.py
@@ -6,25 +6,30 @@ from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    price = models.IntegerField(default=0)
-    stock = models.IntegerField(default=0)
-    description = models.TextField()
-    img_url = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)         # Product name
+    category = models.CharField(max_length=30)      # Product category: Coffee or Tea
+    price = models.IntegerField(default=0)          # Price per piece of product
+    stock = models.IntegerField(default=0)          # Stock available of product
+    description = models.TextField()                # Product description
+    img_url = models.CharField(max_length=200)      # Image file url for showing of product
     def __str__(self):
         return self.name
 
-class Order(models.Model):
-    order_list = ArrayField(models.ForeignKey(Product))
-    amount = ArrayField(models.IntegerField())
-    date = models.DateTimeField(default=datetime.now())
+# In developing    
+    
+# class Order(models.Model):
+#     order_list = ArrayField(models.ForeignKey(Product))
+#     amount = ArrayField(models.IntegerField())
+#     date = models.DateTimeField(default=datetime.now())
 
-class User(models.Model):
-    firstname = models.CharField(max_length=30)
-    lastname = models.CharField(max_length=30)
-    email = models.CharField(max_length=50)
-    password = models.CharFiled(min_length=8, max_length=25)
-    address = models.CharField(max_length=2000)
-    order = ArrayField(models.ForeignKey(Order))
-    cart_product = ArrayField(models.ForeignKey(Product))
-    cart_amount = ArrayField(models.IntegerField())
+# class User(models.Model):
+#     firstname = models.CharField(max_length=30)
+#     lastname = models.CharField(max_length=30)
+#     email = models.CharField(max_length=50)
+#     password = models.CharFiled(min_length=8, max_length=25)
+#     address = models.CharField(max_length=2000)
+#     order = ArrayField(models.ForeignKey(Order))
+#     cart_product = ArrayField(models.ForeignKey(Product))
+#     cart_amount = ArrayField(models.IntegerField())
+#     def __str__(self):
+#         return self.firstname
