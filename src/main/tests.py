c7ee3a11b889b516a62f8e92359692d165c89b9a
Nuttapong Rojanavanich <tuter555awesome@gmail.com>
2017-11-29 16:10:24 +0700

    Add Product attributes test

--- src/main/tests.py
+++ src/main/tests.py
@@ -2,5 +2,19 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from main.models import Product
 
-# Create your tests here.
+class ProductTestCase(TestCase):
+
+    def setUp(self):
+        Product.objects.create(name="test_coffee_1", category="coffee", price=1000, stock=20, description="desc1",
+        img_url="https://i.pinimg.com/originals/d5/1e/ff/d51eff18270586b2b815543ffac32174.jpg")
+
+    def test_get_attributes(self):
+        coffee1 = Product.objects.get(name="test_coffee_1")
+        self.assertEqual(coffee1.name, 'test_coffee_1')
+        self.assertEqual(coffee1.category, 'coffee')
+        self.assertEqual(coffee1.price, 1000)
+        self.assertEqual(coffee1.stock, 20)
+        self.assertEqual(coffee1.description, 'desc1')
+        self.assertEqual(coffee1.img_url, 'https://i.pinimg.com/originals/d5/1e/ff/d51eff18270586b2b815543ffac32174.jpg')
