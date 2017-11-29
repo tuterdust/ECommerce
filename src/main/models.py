47c0adac07d989071b009fde6c967c6a4425a943
Chayanin Punjakunaporn <herofactory.th@gmail.com>
2017-11-29 21:32:35 +0700

    Edit model error again

--- src/main/models.py
+++ src/main/models.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from datetime import datetime
+# from datetime import datetime
 from django.db import models
-from django.contrib.postgres.fields import ArrayField
+# from django.contrib.postgres.fields import ArrayField
 from django.core.validators import RegexValidator
 
 class Product(models.Model):
@@ -16,10 +16,10 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-class Order(models.Model):
-    order_list = ArrayField(models.ForeignKey(Product))
-    amount = ArrayField(models.IntegerField())
-    date = models.DateTimeField(default=datetime.now())
+# class Order(models.Model):
+#     order_list = ArrayField(models.ForeignKey(Product))
+#     amount = ArrayField(models.IntegerField())
+#     date = models.DateTimeField(default=datetime.now())
 
 class User(models.Model):
     firstname = models.CharField(max_length=30)
@@ -27,8 +27,8 @@ class User(models.Model):
     email = models.CharField(max_length=50)
     password = models.CharField(validators=[RegexValidator(regex='^.{8, }$', message='Length has to be 8 or more', code='nomatch')], max_length=25)
     address = models.CharField(max_length=2000)
-    order = ArrayField(models.ForeignKey(Order))
-    cart_product = ArrayField(models.ForeignKey(Product))
-    cart_amount = ArrayField(models.IntegerField())
+    # order = ArrayField(models.ForeignKey(Order))
+    # cart_product = ArrayField(models.ForeignKey(Product))
+    # cart_amount = ArrayField(models.IntegerField())
     def __str__(self):
         return self.firstname
