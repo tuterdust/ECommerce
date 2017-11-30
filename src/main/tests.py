2b56f7c7a3e440b281642707f8a1b50e3f994627
Nuttapong Rojanavanich <tuter555awesome@gmail.com>
2017-11-30 18:28:48 +0700

    Add part of user test case

--- src/main/tests.py
+++ src/main/tests.py
@@ -39,6 +39,7 @@ class SelectedProductTestCase(TestCase):
         self.assertEqual(test_product_1.description, 'desc1')
         self.assertEqual(test_product_1.img_url, 'https://i.pinimg.com/originals/d5/1e/ff/d51eff18270586b2b815543ffac32174.jpg')
         self.assertEqual(test_selected_coffee_1.amount, 3)
+        self.assertEqual(str(test_selected_coffee_1), 'test_coffee_1 X 3')
 
 class OrderTestCase(TestCase):
 
@@ -79,3 +80,46 @@ class OrderTestCase(TestCase):
         self.assertEqual(test_product_1.name, 'test_coffee_1')
         self.assertEqual(test_product_2.name, 'test_coffee_2')
         self.assertEqual(test_product_3.name, 'test_tea_1')
+        self.assertEqual(str(test_order_1), 'Order 1 ')
+
+class UserTestCase(TestCase):
+
+    def setUp(self):
+        test_coffee_1 = Product.objects.create(name="test_coffee_1", category="coffee", price=1000, stock=20, description="desc1",
+        img_url="https://i.pinimg.com/originals/d5/1e/ff/d51eff18270586b2b815543ffac32174.jpg")
+        test_selected_coffee_1 = SelectedProduct.objects.create(product_key=test_coffee_1, amount=3)
+
+        test_coffee_2 = Product.objects.create(name="test_coffee_2", category="coffee", price=1500, stock=23, description="desc2",
+        img_url="https://sc02.alicdn.com/kf/HTB12MG4HVXXXXaDXXXXq6xXFXXXI/221358428/HTB12MG4HVXXXXaDXXXXq6xXFXXXI.jpg")
+        test_selected_coffee_2 = SelectedProduct.objects.create(product_key=test_coffee_2, amount=5)
+
+        test_tea_1 = Product.objects.create(name="test_tea_1", category="tea", price=500, stock=30, description="desc3",
+        img_url="http://lakelandcamel.scene7.com/is/image/LakelandCamel/16967_1")
+        test_selected_tea_1 = SelectedProduct.objects.create(product_key=test_tea_1, amount=2)
+
+        test_selected_coffee_3 = SelectedProduct.objects.create(product_key=test_coffee_1, amount=5)
+        test_selected_tea_2 = SelectedProduct.objects.create(product_key=test_tea_1, amount=3)
+
+        test_order_1 = Order.objects.create()
+        test_order_1.order_list.add(test_selected_coffee_1)
+        test_order_1.order_list.add(test_selected_coffee_2)
+        test_order_1.order_list.add(test_selected_tea_1)
+
+        test_order_2 = Order.objects.create()
+        test_order_2.order_list.add(test_selected_tea_1)
+
+        test_user = User.objects.create(firstname="KFC", lastname="Cookie", email="bnk48@gmail.com", password="koisurufortunecookie01", address="Bangkok")
+        test_user.order_history.add(test_order_1)
+        test_user.order_history.add(test_order_2)
+        test_user.cart_items.add(test_selected_coffee_3)
+        test_user.cart_items.add(test_selected_tea_2)
+
+    def test_get_attributes(self):
+        test_user = User.objects.get(pk=1)
+        self.assertEqual(test_user.firstname, 'KFC')
+        self.assertEqual(test_user.lastname, 'Cookie')
+        self.assertEqual(test_user.email, 'bnk48@gmail.com')
+        self.assertEqual(test_user.password, 'koisurufortunecookie01')
+        self.assertEqual(test_user.address, 'Bangkok')
+        test_order_1 = Order.objects.get(pk=test_user.order_history.all()[0].pk)
+        test_order_2 = Order.objects.get(pk=test_user.order_history.all()[1].pk)
