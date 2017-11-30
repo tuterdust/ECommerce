25a8e84a43b6be622e5903d77ea0d24d3d31dd6c
Nuttapong Rojanavanich <tuter555awesome@gmail.com>
2017-11-30 20:37:00 +0700

    Add more test

--- src/main/testView.py
+++ src/main/testView.py
@@ -51,3 +51,48 @@ class AboutViewTest(TestCase):
          resp = self.client.get(reverse('about'))
          self.assertEqual(resp.status_code, 200)
          self.assertTemplateUsed(resp, 'about.html')
+
+class ContactViewTest(TestCase):
+
+     def test_view_url_exists_at_desired_location(self):
+         resp = self.client.get('/contact/')
+         self.assertEqual(resp.status_code, 200)
+
+     def test_view_url_accessible_by_name(self):
+         resp = self.client.get(reverse('contact'))
+         self.assertEqual(resp.status_code, 200)
+
+     def test_view_use_correct_template(self):
+         resp = self.client.get(reverse('contact'))
+         self.assertEqual(resp.status_code, 200)
+         self.assertTemplateUsed(resp, 'contact.html')
+
+class GuideViewTest(TestCase):
+
+     def test_view_url_exists_at_desired_location(self):
+         resp = self.client.get('/guide/')
+         self.assertEqual(resp.status_code, 200)
+
+     def test_view_url_accessible_by_name(self):
+         resp = self.client.get(reverse('guide'))
+         self.assertEqual(resp.status_code, 200)
+
+     def test_view_use_correct_template(self):
+         resp = self.client.get(reverse('guide'))
+         self.assertEqual(resp.status_code, 200)
+         self.assertTemplateUsed(resp, 'guide.html')
+
+class OrderHistoryViewTest(TestCase):
+
+     def test_view_url_exists_at_desired_location(self):
+         resp = self.client.get('/user/orders/')
+         self.assertEqual(resp.status_code, 200)
+
+     def test_view_url_accessible_by_name(self):
+         resp = self.client.get(reverse('order_history'))
+         self.assertEqual(resp.status_code, 200)
+
+     def test_view_use_correct_template(self):
+         resp = self.client.get(reverse('order_history'))
+         self.assertEqual(resp.status_code, 200)
+         self.assertTemplateUsed(resp, 'order_history.html')
