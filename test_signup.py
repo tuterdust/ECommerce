16f714b38ee3d6619cd44756e5a686649e11608a
jzrlza <parinvut12345@gmail.com>
2017-12-01 05:45:21 +0700

    Added Signup Signin Test

---
+++ test_signup.py
@@ -0,0 +1,62 @@
+import time
+from selenium import webdriver
+
+from selenium.webdriver.support.ui import WebDriverWait
+
+import unittest
+
+class LoginTest(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome("C:/Users/JBCOM/chromedriver_win32/chromedriver.exe")
+        self.driver.get("localhost:8000")
+        
+    def test_Login(self):
+        driver = self.driver
+        print(driver.title)
+
+        
+        signuplink = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_link_text("SIGN UP"))
+
+        signuplink.click()
+
+        strEmail = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id("id_email"))
+        strEmail.send_keys("someone@gmail.com");
+        strPass = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id("id_password"))
+        strPass.send_keys("12345678");
+        strPassConfirm = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id("id_confirm_password"))
+        strPassConfirm.send_keys("12345678");
+        strName = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id("id_firstname"))
+        strName.send_keys("Some");
+        strLastName = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id("id_lastname"))
+        strLastName.send_keys("One");
+        strAddress = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id("id_address"))
+        strAddress.send_keys("SomePlace");
+
+        btnsubmit = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath("/html/body/div[2]/center/form/div[2]/div[2]/input"))
+        btnsubmit.click()
+
+        signinlink = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_link_text("SIGN IN"))
+        signinlink.click()
+
+        inEmail = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id("id_email"))
+        inEmail.send_keys("someone@gmail.com");
+        inPass = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id("id_password"))
+        inPass.send_keys("12345678");
+
+        btnsignin = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath("/html/body/div[2]/center/div/div[2]/form/input[2]"))
+        btnsignin.click()
+
+        WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath("/html/body/div[2]/div[1]/div[1]/img"))
+        
+        time.sleep(8)
+        
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
+
+# Test Lists
+# 1) Check if CART button link works or not
+# 2) ...
