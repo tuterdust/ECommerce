3982c546b339837c1a8658dc9fe736d32f95fed4
Chayanin Punjakunaporn <herofactory.th@gmail.com>
2017-11-30 09:31:49 +0700

    Fix db

--- src/main/admin.py
+++ src/main/admin.py
@@ -2,7 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Product, User
+from .models import Product, SelectedProduct, Order, User
 
 admin.site.register(Product)
+
+#Debugging
+admin.site.register(SelectedProduct)
+admin.site.register(Order)
 admin.site.register(User)
