4751645b3e469b413709dc88faef2018c208365d
Chayanin Punjakunaporn <herofactory.th@gmail.com>
2017-11-30 21:35:28 +0700

    Make checking same email in db

--- src/main/views.py
+++ src/main/views.py
@@ -102,8 +102,14 @@ def sign_up(request):
             if password != confirm_password:
                 context = { "form": form, "sign_up_error": "Wrong confirming password" }
                 return render(request, 'sign_up.html', context)
+            same_email_user = User.objects.filter(email=email)
+            if same_email_user:
+                context = { "form": form, "sign_up_error": "This email has been used" }
+                return render(request, 'sign_up.html', context)
             # TODO: Save new user
             # Don't forget to check that email is used in other users or not.
+            # u = User(email=email, password=password, firstname=firstname, lastname=lastname, address=address)
+            # u.save()
             context = { "sign_up_complete": True }
             return render(request, 'home.html', context)
         else:
